b1cb386ae3cf77e2a14f900d772f8971727755c8
LlanellaWhatCake <amandathompsonalt@gmail.com>
2022-02-16 19:03:59 -0500

    Changed gitignore, notification css

--- src/Components/Notification.js
+++ src/Components/Notification.js
@@ -14,65 +14,77 @@ import Avatar from '@mui/material/Avatar';
 
 export const Notification = (props) => {
 
-    const { username, avatarSrc, notifSummary, notifDetails, deviationSrc } = props;
-    const [selected, setSelected] = useState(false); //change to whatever the props say later
-    const [styleContext, setStyleContext] = useContext(StyleContext);
+  const { username, avatarSrc, notifSummary, notifDetails, deviationSrc } = props;
+  const [selected, setSelected] = useState(false); //change to whatever the props say later
+  const [styleContext, setStyleContext] = useContext(StyleContext);
 
 
-    const toggleCheckbox = checked => {
-      setSelected(checked);
+  const toggleCheckbox = checked => {
+    setSelected(checked);
+  }
+
+  const style = {
+    container: {
+      display: "flex",
+      justifyContent: "space-around",
+      margin: "10px 0",
+      padding: "10px 0",
+      background: styleContext.theme.palette.backgroundThird.main
+    },
+    notifMainContent: {
+      width: "100%",
+      display: "flex",
+      justifyContent: "space-around"
+    },
+    checkbox: {
+      margin: "0 20px",
+      '& .MuiSvgIcon-root': {
+        fontSize: 35
+      }
+    },
+    avatar: {
+      margin: "0 20px",
+      width: 56,
+      height: 56
+    },
+    informationBlock: {
+      width: "100%",
+      margin: "0 30px",
+      color: styleContext.theme.palette.textMain.light
+    },
+    notifDetails: {
+      background: styleContext.theme.palette.backgroundSecondary.main,
+      padding: "5px 10px",
+      maxHeight: "75px"
+    },
+    deviationPreview: {
+      backgroundImage: `url(${deviationSrc})`,
+      background: "purple",
+      margin: "0 10px",
+      width: "300px",
+      height: "100px" 
     }
+  }
 
-    const style = {
-        container: {
-            display: "flex",
-            justifyContent: "space-around",
-            margin: "10px 0",
-            padding: "10px 0",
-            background: styleContext.theme.palette.backgroundThird.main
-        },
-        notifMainContent: {
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-around"
-        },
-        checkbox: {
-          margin: "0 20px",
-         '& .MuiSvgIcon-root': { 
-           fontSize: 35 
-          }
-        },
-        avatar: {
-          margin: "0 20px",
-          width: 56, 
-          height: 56 
-        },
-        informationBlock: {
-          width: "100%",
-          margin: "0 30px"
-        },
-        notifDetails: {
-          background: styleContext.theme.palette.backgroundSecondary.main
-        }
-    }
-  
 
   return (
     <Paper sx={{ ...style.container }}>
       <div style={{ ...style.notifMainContent }}>
-        <Checkbox checked={selected} sx={{ ...style.checkbox }}  onClick={({target: { checked }}) => {
+        <Checkbox checked={selected} sx={{ ...style.checkbox }} onClick={({ target: { checked } }) => {
           toggleCheckbox(checked);
         }} />
         <Avatar src={avatarSrc} variant="rounded" sx={{ ...style.avatar }} />
         <div style={{ ...style.informationBlock }}>
-        {notifSummary}
-        <div style={{ ...style.notifDetails }}>
-          {notifDetails}
-        </div>
+          {notifSummary}
+          <div style={{ ...style.notifDetails }}>
+            {notifDetails}
           </div>
-      
+          Reply
+        </div>
+        <Box sx={{ ...style.deviationPreview }} />
+
       </div>
-        
+
     </Paper>
 
   );
